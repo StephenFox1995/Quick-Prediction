ef59ed108caf6395b1b3e62b54036f0001bf8248
Stephen Fox <stephenfox995@hotmail.com>
2016-10-26 22:22:21 +0100

    Added date functions

--- dbexecute.py
+++ dbexecute.py
@@ -8,9 +8,6 @@ import time
 
 # current date  -3 months, do each quarter.
 
-DATE_TIME_FORMAT = "%d/%m/%Y %H:%M"
-
-
 
 def extractTime(previous, current):
   previous.append(current["createdAt"])
@@ -23,12 +20,25 @@ def threeMonthsAgo():
   return datetime.today() + relativedelta(months=-3)
 
 
+def dateRange(start, end, increment, period):
+# http://stackoverflow.com/a/10688309/2875074
+    result = []
+    nxt = start
+    delta = relativedelta(**{period:increment})
+    while nxt <= end:
+        result.append(nxt)
+        nxt += delta
+    return result
+
+
 def getOrdersForDay(day, orders):
   """
   Get amount of orders for a day. This function calculates this
   by checking if there is two objects in the array with the same
   date. If the objects have the same date, then they are assumed
   to be an order for that day, thus get added to the array.
+  @param day: () The day to filter with the dataset.
+  @param orders: (array) The dataset of orders. 
   """
   def filterDays(dayToCompare):
     DATE_FORMAT = "%d/%m/%Y"
@@ -36,8 +46,7 @@ def getOrdersForDay(day, orders):
     dateTwo = datetime.strftime(dayToCompare, DATE_FORMAT)  
     if dateOne == dateTwo:
       return dateTwo
-  filtered = filter(filterDays, orders)
-  return filtered 
+  return filter(filterDays, orders) 
 
 
 def getOrdersForHour(hour, orders):
@@ -54,8 +63,16 @@ def getOrdersForHour(hour, orders):
     hourTwo = datetime.strftime(hourToCompare, HOUR_FORMAT)
     if hourOne == hourTwo:
       return hourTwo
-  filtered = filter(filterHours, orders)
-  return filtered
+  return filter(filterHours, orders)
+
+
+def getAllUniqueDates(dates):
+  DATE_FORMAT = "%d/%m/%Y"
+  
+  return map(lambda c: datetime.strptime(c, DATE_FORMAT), \
+    reduce(lambda l, x: l.append(datetime.strftime(x, DATE_FORMAT))  
+      or l if datetime.strftime(x, DATE_FORMAT) not in l else l, dates, []))
+
 
 
 database = Database()
@@ -64,6 +81,7 @@ db = database.getDB()
 # Get the orders from the database
 orders = db.orders.find({"createdAt": {"$gte": threeMonthsAgo()}})
 
+r = dateRange(threeMonthsAgo(), datetime.today(), 1, 'days')
 
 # Build up rows for .csv file
 rows = []
@@ -75,6 +93,7 @@ for order in orders:
 # Get all timestamps
 orderTimeStamps = reduce(extractTime, ordersArray, [])
 
-ordersForDay = getOrdersForDay(orderTimeStamps[34], orderTimeStamps)
+uniqueDays = getAllUniqueDates(orderTimeStamps)
+ordersForDay = getOrdersForDay(uniqueDays[0], orderTimeStamps)
+print(len(getOrdersForHour(ordersForDay[4], ordersForDay)))
 
-print(len(getOrdersForHour(ordersForDay[5], ordersForDay)))
