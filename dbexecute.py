527585fedaf709ddb3627543b26e62328fd78de0
Stephen Fox <stephenfox995@hotmail.com>
2016-10-27 00:47:18 +0100

    Every hour for the previous 3 months will contain a value for the amount of orders for that hour

--- dbexecute.py
+++ dbexecute.py
@@ -3,18 +3,39 @@ from bson.objectid import ObjectId
 from qoutput import QOutput
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-import time
+
 
 
 # current date  -3 months, do each quarter.
 
+# hours in datetime format
+hours = map(lambda hour: datetime.strptime(hour, "%H:%M") ,[
+  "00:00",
+  "01:00",
+  "02:00",
+  "03:00",
+  "04:00",
+  "05:00",
+  "06:00",
+  "07:00",
+  "08:00",
+  "09:00",
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+  "17:00",
+  "18:00",
+  "19:00",
+  "20:00",
+  "21:00",
+  "22:00",
+  "23:00"
+])
 
-def extractTime(previous, current):
-  previous.append(current["createdAt"])
-  return previous
-
-def currentDate():
-  return time.strftime("%d/%m/%Y")
 
 def threeMonthsAgo():
   return datetime.today() + relativedelta(months=-3)
@@ -31,7 +52,7 @@ def dateRange(start, end, increment, period):
     return result
 
 
-def getOrdersForDay(day, orders):
+def getOrdersForDate(date, orders):
   """
   Get amount of orders for a day. This function calculates this
   by checking if there is two objects in the array with the same
@@ -42,7 +63,7 @@ def getOrdersForDay(day, orders):
   """
   def filterDays(dayToCompare):
     DATE_FORMAT = "%d/%m/%Y"
-    dateOne = datetime.strftime(day, DATE_FORMAT)
+    dateOne = datetime.strftime(date, DATE_FORMAT)
     dateTwo = datetime.strftime(dayToCompare, DATE_FORMAT)  
     if dateOne == dateTwo:
       return dateTwo
@@ -74,26 +95,65 @@ def getAllUniqueDates(dates):
       or l if datetime.strftime(x, DATE_FORMAT) not in l else l, dates, []))
 
 
+def getDateTimesFromMongoOrderData(orders):
+  def extractTime(current):
+    return current["createdAt"]
+  # Get all timestamps from mongo
+  return  map(extractTime, orders)
+
+def zeroFillOrdersForFullDay():
+   return map(lambda c: {c: 0}, hours)
+  
+
 
 database = Database()
 db = database.getDB()
 
 # Get the orders from the database
 orders = db.orders.find({"createdAt": {"$gte": threeMonthsAgo()}})
+# Get all date times from orders
+orderDates = getDateTimesFromMongoOrderData(orders)
+# Get every date between today and three months ago.
+threeMonthDateRange = dateRange(threeMonthsAgo(), datetime.today(), 1, 'days')
 
-r = dateRange(threeMonthsAgo(), datetime.today(), 1, 'days')
+# All the details about orders for the previous three months.
+orderDetailsForThreeMonths = []
+
+# Loop through array of dates from previous three months
+# check the amount of orders per hour and add to dict.
+for date in threeMonthDateRange:
+  print("date: %s" % (datetime.strftime(date, "%d/%m/%Y")))
+  orderDetails = {
+    "date": object,
+    "orders": []
+  }
+  orderDetails["date"] = date
+
+  ordersForDate = getOrdersForDate(date, orderDates)
+  # if the order amount for that date is zero then just fill all hours with a order count of 0
+  if len(ordersForDate) == 0:
+    orderDetails["orders"] = zeroFillOrdersForFullDay()
+    orderDetailsForThreeMonths.append(orderDetails)
+    continue
+  
+  
+  # If there was orders for that date get orders for each hour
+  for hour in hours:
+    ordersAmountForHour = len(getOrdersForHour(hour, ordersForDate))
+    # As each hour only contains XX:XX, it doesn't have a date.
+    # Combine the current hour iteration with the current date iteration 
+    hour = datetime.combine(date, datetime.time(hour))
+    if ordersAmountForHour == 0:
+      ordersForHour = {hour: 0}
+      orderDetails["orders"].append(ordersForHour) 
+    else:
+      ordersForHour = {hour: ordersAmountForHour}
+      orderDetails["orders"].append(ordersForHour)
+  orderDetailsForThreeMonths.append(orderDetails)
+      
+print "p"
 
 # Build up rows for .csv file
-rows = []
-ordersArray = []
-# Add all orders to ordersArray
-for order in orders:
-  ordersArray.append(order)
+# output = QOutput("previous_three_months", ["timestamp"])
 
-# Get all timestamps
-orderTimeStamps = reduce(extractTime, ordersArray, [])
-
-uniqueDays = getAllUniqueDates(orderTimeStamps)
-ordersForDay = getOrdersForDay(uniqueDays[0], orderTimeStamps)
-print(len(getOrdersForHour(ordersForDay[4], ordersForDay)))
 
