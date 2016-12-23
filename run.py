cc167dbd8a749e12fc88edfc2b485638609622f3
Stephen Fox <stephenfox995@hotmail.com>
2016-12-23 23:04:52 +0000

    renamed setup.py to run.py. monthsprior commandline arg can now be specified

--- run.py
+++ run.py
@@ -15,14 +15,21 @@ def args():
   parser = argparse.ArgumentParser("Execute swarms and models.")
   parser.add_argument(
     "-s", "--swarmtype", 
-          help="The swarm type to perform [orderamount, producttype]", 
+          help="The swarm type to perform [orderamount, producttype].", 
           dest="swarmtype", 
           choices=set(("orderamount", "producttype"))
   )
   parser.add_argument(
     "-b", "--businessid",
-        help="The id of the business",
+        help="The id of the business.",
         dest="businessid"
+  )
+  parser.add_argument(
+    "-m", "-monthsprior",
+        help="How far back data from the database should be fetched in months.",
+        dest="monthsprior",
+        type=int,
+        default=-3
   )
   return parser
 
@@ -31,15 +38,17 @@ if __name__ == "__main__":
   args = args().parse_args()
   swarmType = args.swarmtype.upper()
   businessid = args.businessid
-
+  monthsprior = monthRangeFrom(args.monthsprior)
+  print(monthsprior)
+  
   # Connect to the database
   mongoDetails = mongoconfig.getMongoDetails()
   database = Database(mongoDetails["uri"], mongoDetails["port"], mongoDetails["database"])
   database.connect()
   # Get orders from three months ago.
-  orders = database.getOrders(monthRangeFrom(months=-3))
+  orders = database.getOrders(monthsprior)
   # Parse out the number of orders for each hour of the last three months.
-  hourlyOrders = TimeParser.extractHourlyOrders(orders, monthRangeFrom(months=-3))
+  hourlyOrders = TimeParser.extractHourlyOrders(orders, monthsprior)
   database.close()
   # Get ready to write to .csv file
   predict = Predict(businessid, swarmType)
